f60b31be162d26293ed258f435d1d1627fcffd96
= <dcnation1000000@gmail.com>
2022-12-22 19:51:26 -0500

    complete task 4

--- index.js
+++ index.js
@@ -70,19 +70,27 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+let totalContributions =  GAMES_JSON.reduce((total, x) => {
+    return total + x.backers;
+}, 0)
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `${totalContributions.toLocaleString('en-US')}`
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+let totalPledged = GAMES_JSON.reduce((total, x) => {
+    return total + x.pledged;
+}, 0)
 
+// set inner HTML using template literal
+raisedCard.innerHTML = `$${totalPledged.toLocaleString('en-us')}`
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+
+gamesCard.innerHTML = `${GAMES_JSON.length}`
 
 
 /*************************************************************************************
